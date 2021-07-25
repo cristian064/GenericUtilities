52af9fa5bca35aaad8e02aaab24492784e52285b
cristian <cristian.ayala.064@gmail.com>
2021-07-24 22:07:44 -0500

    setup min version iOS13

---
+++ Sources/GenericUtilities/Extension+MKMarkerAnnotationView.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cristian Ayala Laura on 24/07/21.
+//
+
+import MapKit
+
+extension MKMarkerAnnotationView {
+    static var identifier : String{
+        return "\(String(describing: self))Identifier"
+    }
+}
